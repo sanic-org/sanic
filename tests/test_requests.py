a4ed6a39238de57b01678f40c87863d20102aa77
Sean Parsons <seanpatrick2013@gmail.com>
2016-12-25 18:35:29 -0500

    Added 2 more asserts to test_post_token for Bearer & Basic.

--- tests/test_requests.py
+++ tests/test_requests.py
@@ -134,6 +134,32 @@ def test_post_token():
     assert request.token == token
     assert response.text == 'OK'
 
+    # uuid4 generated token.
+    token = 'a1d895e0-553a-421a-8e22'
+
+    headers = {
+        'content-type': 'application/json',
+        'Authorization': 'Bearer {}'.format(token)
+    }
+
+    request, response = sanic_endpoint_test(app, data=json_dumps(payload), headers=headers)
+
+    assert request.token == token
+    assert response.text == 'OK'
+
+    # uuid4 generated token.
+    token = '421a-8e22-5ff8ecb48cbf'
+
+    headers = {
+        'content-type': 'application/json',
+        'Authorization': 'Basic {}'.format(token)
+    }
+
+    request, response = sanic_endpoint_test(app, data=json_dumps(payload), headers=headers)
+
+    assert request.token == token
+    assert response.text == 'OK'
+
 
 def test_post_form_urlencoded():
     app = Sanic('test_post_form_urlencoded')

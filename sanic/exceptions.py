68f6553181df0a824295291483ec09e71f693c9e
Eli Uriegas <seemethere101@gmail.com>
2016-12-25 10:37:23 -0800

    Add invalid response object error handling

--- sanic/exceptions.py
+++ sanic/exceptions.py
@@ -21,6 +21,10 @@ class ServerError(SanicException):
     status_code = 500
 
 
+class InvalidResponseObject(SanicException):
+    status_code = 500
+
+
 class FileNotFound(NotFound):
     status_code = 404
 

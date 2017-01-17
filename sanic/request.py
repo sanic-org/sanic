5903dd293909c56c697fa7972303028afb7e3884
Guilherme Polo <gp@luster.cc>
2017-01-17 02:58:45 -0200

    cannot use the new .ip without updating __slots__

--- sanic/request.py
+++ sanic/request.py
@@ -44,6 +44,7 @@ class Request(dict):
         'url', 'headers', 'version', 'method', '_cookies', 'transport',
         'query_string', 'body',
         'parsed_json', 'parsed_args', 'parsed_form', 'parsed_files',
+        '_ip',
     )
 
     def __init__(self, url_bytes, headers, version, method, transport):

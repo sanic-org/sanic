a1ee8f076e984b67d29c6f9fbc74f9416b294874
Raphael Deem <raphael.deem@gmail.com>
2017-01-28 15:26:44 -0800

    fix async run

--- sanic/server.py
+++ sanic/server.py
@@ -297,8 +297,8 @@ def serve(host, port, request_handler, error_handler, before_start=None,
     :param protocol: Subclass of asyncio protocol class
     :return: Nothing
     """
-    loop = async_loop.new_event_loop()
-    asyncio.set_event_loop(loop)
+    loop = asyncio.get_event_loop()
+    asyncio.set_event_loop_policy(async_loop.EventLoopPolicy())
 
     if debug:
         loop.set_debug(debug)

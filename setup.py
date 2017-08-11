cb985fb5f25e6605f8a53c8804a74a3a13e9e1fc
AZLisme <helloazl@icloud.com>
2017-08-11 14:15:08 +0800

    add more requirments to setup.py, sorry forget that

--- setup.py
+++ setup.py
@@ -60,6 +60,7 @@ requirements = [
     ujson,
     'aiofiles>=0.3.0',
     'websockets>=3.2',
+    'itsdangerous>=0.24'
 ]
 if strtobool(os.environ.get("SANIC_NO_UJSON", "no")):
     print("Installing without uJSON")

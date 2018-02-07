5ef567405fdf7ef476fb4683fd8e455d4f762db2
Alec Buckenheimer <abuckenheimer@autonomycapital.com>
2018-02-06 19:56:25 -0500

    fixed platform from windows to win32

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ setup_kwargs = {
     ],
 }
 
-env_dependency = '; sys_platform != "windows" and implementation_name == "cpython"'
+env_dependency = '; sys_platform != "win32" and implementation_name == "cpython"'
 ujson = 'ujson>=1.35' + env_dependency
 uvloop = 'uvloop>=0.5.3' + env_dependency
 

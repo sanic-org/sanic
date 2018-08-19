f38099513fb8e393049c400f71b1264d837c6f79
Oliver <hanleilei@gmail.com>
2018-08-19 21:18:03 +0800

    fix packaging module do not have version attribute

--- tests/test_request_timeout.py
+++ tests/test_request_timeout.py
@@ -9,15 +9,12 @@ from aiohttp import TCPConnector, ClientResponse
 from sanic.testing import SanicTestClient, HOST, PORT
 
 try:
+    # setuptools v39.0 and above.
     try:
-        import packaging # direct use
+        from setuptools.extern import packaging
     except ImportError:
-        # setuptools v39.0 and above.
-        try:
-            from setuptools.extern import packaging
-        except ImportError:
-            # Before setuptools v39.0
-            from pkg_resources.extern import packaging
+        # Before setuptools v39.0
+        from pkg_resources.extern import packaging
     version = packaging.version
 except ImportError:
     raise RuntimeError("The 'packaging' library is missing.")

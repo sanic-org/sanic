2c54f2449f4a73b86d58824f4869d72f1a2f2a04
Harsha Narayana <harsha2k4@gmail.com>
2018-11-06 21:46:41 +0530

    add documentation and additional unit tests
    Signed-off-by: Harsha Narayana <harsha2k4@gmail.com>

--- tests/test_blueprint_group.py
+++ tests/test_blueprint_group.py
@@ -2,12 +2,16 @@ from pytest import raises
 
 from sanic.app import Sanic
 from sanic.blueprints import Blueprint
-from sanic.response import text
+from sanic.response import text, HTTPResponse
+from sanic.request import Request
+from sanic.exceptions import Unauthorized
 
 MIDDLEWARE_INVOKE_COUNTER = {
-        'request': 0,
-        'response': 0
-    }
+    'request': 0,
+    'response': 0
+}
+
+AUTH = "dGVzdDp0ZXN0Cg=="
 
 
 def test_bp_group_indexing(app: Sanic):
@@ -19,6 +23,78 @@ def test_bp_group_indexing(app: Sanic):
 
     with raises(expected_exception=IndexError) as e:
         _ = group[3]
+
+
+def test_bp_group_with_additional_route_params(app: Sanic):
+    blueprint_1 = Blueprint('blueprint_1', url_prefix="/bp1")
+    blueprint_2 = Blueprint('blueprint_2', url_prefix='/bp2')
+
+    @blueprint_1.route(
+        "/request_path",
+        methods=frozenset({"PUT", "POST"}),
+        version=2
+    )
+    def blueprint_1_v2_method_with_put_and_post(request: Request):
+        if request.method == "PUT":
+            return text("PUT_OK")
+        elif request.method == "POST":
+            return text("POST_OK")
+
+    @blueprint_2.route(
+        "/route/<param>",
+        methods=frozenset({"DELETE", "PATCH"}),
+        name="test"
+    )
+    def blueprint_2_named_method(request: Request, param):
+        if request.method == "DELETE":
+            return text("DELETE_{}".format(param))
+        elif request.method == "PATCH":
+            return text("PATCH_{}".format(param))
+
+    blueprint_group = Blueprint.group(
+        blueprint_1,
+        blueprint_2,
+        url_prefix="/api"
+    )
+
+    @blueprint_group.middleware("request")
+    def authenticate_request(request: Request):
+        global AUTH
+        auth = request.headers.get("authorization")
+        if auth:
+            # Dummy auth check. We can have anything here and it's fine.
+            if AUTH not in auth:
+                return text("Unauthorized", status=401)
+        else:
+            return text("Unauthorized", status=401)
+
+    @blueprint_group.middleware("response")
+    def enhance_response_middleware(
+            request: Request,
+            response: HTTPResponse
+    ):
+        response.headers.add("x-test-middleware", "value")
+
+    app.blueprint(blueprint_group)
+
+    header = {
+        'authorization': " ".join(["Basic", AUTH])
+    }
+    _, response = app.test_client.put("/v2/api/bp1/request_path", headers=header)
+    assert response.text == "PUT_OK"
+    assert response.headers.get("x-test-middleware") == "value"
+
+    _, response = app.test_client.post("/v2/api/bp1/request_path", headers=header)
+    assert response.text == "POST_OK"
+
+    _, response = app.test_client.delete("/api/bp2/route/bp2", headers=header)
+    assert response.text == "DELETE_bp2"
+
+    _, response = app.test_client.patch("/api/bp2/route/bp2", headers=header)
+    assert response.text == "PATCH_bp2"
+
+    _, response = app.test_client.get("/v2/api/bp1/request_path")
+    assert response.status == 401
 
 
 def test_bp_group(app: Sanic):

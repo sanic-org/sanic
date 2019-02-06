08794ae1cfb0eadc000fbdbd1f3185eeb8965125
Leonardo Teixeira Menezes <leonardotmenezes@gmail.com>
2019-02-06 16:29:33 -0200

    Enforce Datetime Type for Expires on Set-Cookie (#1484)
    * Enforce Datetime Type for Expires on Set-Cookie
    
    * Fix lint issues
    
    * Format code and improve error type
    
    * Fix import order

--- tests/test_app.py
+++ tests/test_app.py
@@ -12,6 +12,7 @@ from sanic.response import text
 def uvloop_installed():
     try:
         import uvloop
+
         return True
     except ImportError:
         return False
@@ -27,28 +28,28 @@ def test_app_loop_running(app):
     assert response.text == "pass"
 
 
-@pytest.mark.skipif(sys.version_info < (3, 7),
-                    reason="requires python3.7 or higher")
+@pytest.mark.skipif(
+    sys.version_info < (3, 7), reason="requires python3.7 or higher"
+)
 def test_create_asyncio_server(app):
     if not uvloop_installed():
         loop = asyncio.get_event_loop()
-        asyncio_srv_coro = app.create_server(
-            return_asyncio_server=True)
+        asyncio_srv_coro = app.create_server(return_asyncio_server=True)
         assert isawaitable(asyncio_srv_coro)
         srv = loop.run_until_complete(asyncio_srv_coro)
         assert srv.is_serving() is True
 
 
-@pytest.mark.skipif(sys.version_info < (3, 7),
-                    reason="requires python3.7 or higher")
+@pytest.mark.skipif(
+    sys.version_info < (3, 7), reason="requires python3.7 or higher"
+)
 def test_asyncio_server_start_serving(app):
     if not uvloop_installed():
         loop = asyncio.get_event_loop()
         asyncio_srv_coro = app.create_server(
             return_asyncio_server=True,
-            asyncio_server_kwargs=dict(
-                start_serving=False
-            ))
+            asyncio_server_kwargs=dict(start_serving=False),
+        )
         srv = loop.run_until_complete(asyncio_srv_coro)
         assert srv.is_serving() is False
 

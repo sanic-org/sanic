910d49a47f44699279d722a96257c105770b5e38
Zaar Hai <haizaar@haizaar.com>
2019-04-02 15:10:22 +1100

    strtobool should actually return bool

--- sanic/config.py
+++ sanic/config.py
@@ -125,7 +125,7 @@ class Config(dict):
                         self[config_key] = float(v)
                     except ValueError:
                         try:
-                            self[config_key] = bool(strtobool(v))
+                            self[config_key] = strtobool(v)
                         except ValueError:
                             self[config_key] = v
 
@@ -134,11 +134,14 @@ def strtobool(val):
     """
     This function was borrowed from distutils.utils. While distutils
     is part of stdlib, it feels odd to use distutils in main application code.
+
+    The function was modified to walk its talk and actually return bool
+    and not int.
     """
     val = val.lower()
     if val in ("y", "yes", "t", "true", "on", "1"):
-        return 1
+        return True
     elif val in ("n", "no", "f", "false", "off", "0"):
-        return 0
+        return False
     else:
         raise ValueError("invalid truth value %r" % (val,))

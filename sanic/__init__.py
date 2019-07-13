a019ff61e371b1775c25a86f9b8d3901756a1f46
Harsha Narayana <harsha2k4@gmail.com>
2019-07-13 13:29:17 +0530

    fix: #1631: linter fix and tox platform selector
    Signed-off-by: Harsha Narayana <harsha2k4@gmail.com>

--- sanic/__init__.py
+++ sanic/__init__.py
@@ -1,5 +1,6 @@
+from sanic.__version__ import __version__
 from sanic.app import Sanic
 from sanic.blueprints import Blueprint
-from sanic.__version__ import __version__
 
-__all__ = ["Sanic", "Blueprint"]
+
+__all__ = ["Sanic", "Blueprint", "__version__"]

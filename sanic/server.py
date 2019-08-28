ae91852cd586c67ddfe10669eed18b3ded332d2f
Subham Roy <syfluqs@gmail.com>
2019-08-28 22:00:23 +0530

    check for already set asyncio event loop policy (#1637)
    * check for already set asyncio event loop policy
    
    * fix linting warning

--- sanic/server.py
+++ sanic/server.py
@@ -30,7 +30,8 @@ from sanic.response import HTTPResponse
 try:
     import uvloop
 
-    asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+    if not isinstance(asyncio.get_event_loop_policy(), uvloop.EventLoopPolicy):
+        asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 except ImportError:
     pass
 

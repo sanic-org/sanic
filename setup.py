99a7e9b9b19f0953177ec75351099ff2b49c7068
Adam Hopkins <admhpkns@gmail.com>
2020-01-03 00:05:44 +0200

    Add Python 3.8 to tests and package classifiers
    Add Python3.8 to Appveyor config

--- setup.py
+++ setup.py
@@ -68,6 +68,7 @@ setup_kwargs = {
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
 }
 

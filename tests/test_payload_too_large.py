d4cba118997eeefbaa7b3ed1300a58bf2c0434f1
L. Kärkkäinen <tronic@users.noreply.github.com>
2020-02-21 09:37:41 +0200

    Add test for async handler of payload too large.

--- tests/test_payload_too_large.py
+++ tests/test_payload_too_large.py
@@ -18,6 +18,22 @@ def test_payload_too_large_from_error_handler(app):
     assert response.text == "Payload Too Large from error_handler."
 
 
+def test_payload_too_large_from_async_error_handler(app):
+    app.config.REQUEST_MAX_SIZE = 1
+
+    @app.route("/1")
+    async def handler1(request):
+        return text("OK")
+
+    @app.exception(PayloadTooLarge)
+    async def handler_exception(request, exception):
+        return text("Payload Too Large from error_handler.", 413)
+
+    response = app.test_client.get("/1", gather_request=False)
+    assert response.status == 413
+    assert response.text == "Payload Too Large from error_handler."
+
+
 def test_payload_too_large_at_data_received_default(app):
     app.config.REQUEST_MAX_SIZE = 1
 

938c49b899595b1b2f980109508311d9631cd465
Adam Hopkins <admhpkns@gmail.com>
2020-06-28 14:45:52 +0300

    Add handler names for websockets for url_for usage (#1880)

--- tests/test_url_for.py
+++ tests/test_url_for.py
@@ -1,3 +1,8 @@
+import asyncio
+
+from sanic.blueprints import Blueprint
+
+
 def test_routes_with_host(app):
     @app.route("/")
     @app.route("/", name="hostindex", host="example.com")
@@ -13,3 +18,46 @@ def test_routes_with_host(app):
         app.url_for("hostpath", _external=True)
         == "http://path.example.com/path"
     )
+
+
+def test_websocket_bp_route_name(app):
+    """Tests that blueprint websocket route is named."""
+    event = asyncio.Event()
+    bp = Blueprint("test_bp", url_prefix="/bp")
+
+    @bp.get("/main")
+    async def main(request):
+        ...
+
+    @bp.websocket("/route")
+    async def test_route(request, ws):
+        event.set()
+
+    @bp.websocket("/route2")
+    async def test_route2(request, ws):
+        event.set()
+
+    @bp.websocket("/route3", name="foobar_3")
+    async def test_route3(request, ws):
+        event.set()
+
+    app.blueprint(bp)
+
+    uri = app.url_for("test_bp.main")
+    assert uri == "/bp/main"
+
+    uri = app.url_for("test_bp.test_route")
+    assert uri == "/bp/route"
+    request, response = app.test_client.websocket(uri)
+    assert response.opened is True
+    assert event.is_set()
+
+    event.clear()
+    uri = app.url_for("test_bp.test_route2")
+    assert uri == "/bp/route2"
+    request, response = app.test_client.websocket(uri)
+    assert response.opened is True
+    assert event.is_set()
+
+    uri = app.url_for("test_bp.foobar_3")
+    assert uri == "/bp/route3"

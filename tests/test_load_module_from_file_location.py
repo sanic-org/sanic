3f52b13b5455ccad279a18376ec16f1e89d81dbb
tomaszdrozdz <tomasz.drozdz.1@protonmail.com>
2020-09-01 20:37:20 +0200

    Working on unit tests.

--- tests/test_load_module_from_file_location.py
+++ tests/test_load_module_from_file_location.py
@@ -7,20 +7,16 @@ from sanic.exceptions import LoadFileException
 from sanic.utils import load_module_from_file_location
 
 
-def test_load_module_from_file_location(loaded_module):
+def test_load_module_from_file_location():
     assert isinstance(
         load_module_from_file_location(
-            Path(__file__).parent / "static/app_conf.py"
-        ),
-        ModuleType,
-    )
+            Path(__file__).parent / "static/app_conf.py"),
+        ModuleType)
 
 
-def test_load_module_from_file_location_with_non_existing_env_variable(
-    loaded_module,
-):
+def test_load_module_from_file_location_with_non_existing_env_variable():
     with pytest_raises(
         LoadFileException,
-        match="The following environment variables are not set: {MuuMilk}",
-    ):
+        match="The following environment variables are not set: {MuuMilk}"):
+
         load_module_from_file_location("${MuuMilk}")

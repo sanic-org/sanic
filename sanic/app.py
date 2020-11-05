3e4bec7f2cc3c9ed1ffd84ce51e77d6f0f3acb21
Ashley Sommer <ashleysommer@gmail.com>
2020-11-05 14:53:48 +1000

    Fix Chunked Transport-Encoding in ASGI streaming response In ASGI-mode, don't do sanic-side response chunk encoding, leave that to the ASGI-response-transport Don't set content-length when using chunked-encoding in ASGI mode, this is incompatible with ASGI Chunked Transport-Encoding.
    (cherry picked from commit c0839afddea13501a3098f8e27e686a6a95a71ee)

--- sanic/app.py
+++ sanic/app.py
@@ -1474,3 +1474,4 @@ class Sanic:
         self.asgi = True
         asgi_app = await ASGIApp.create(self, scope, receive, send)
         await asgi_app()
+    _asgi_single_callable = True  # We conform to ASGI 3.0 single-callable

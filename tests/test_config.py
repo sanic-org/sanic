530375a095e06830ca581b2d23bdcf54041ef1d5
Adam Hopkins <admhpkns@gmail.com>
2021-01-19 00:00:14 +0200

    Remove pytest-dependency

--- tests/test_config.py
+++ tests/test_config.py
@@ -288,3 +288,30 @@ def test_config_rewrite_keep_alive():
     assert config.KEEP_ALIVE is False
     config = Config(defaults={"KEEP_ALIVE": True})
     assert config.KEEP_ALIVE is True
+
+
+_test_setting_as_dict = {"TEST_SETTING_VALUE": 1}
+_test_setting_as_class = type("C", (), {"TEST_SETTING_VALUE": 1})
+_test_setting_as_module = str(
+    Path(__file__).parent / "static/app_test_config.py"
+)
+
+
+@pytest.mark.parametrize(
+    "conf_object",
+    [
+        _test_setting_as_dict,
+        _test_setting_as_class,
+        _test_setting_as_module,
+    ],
+    ids=["from_dict", "from_class", "from_file"],
+)
+def test_update(app, conf_object):
+    app.update_config(conf_object)
+    assert app.config["TEST_SETTING_VALUE"] == 1
+
+
+def test_update_from_lowercase_key(app):
+    d = {"test_setting_value": 1}
+    app.update_config(d)
+    assert "test_setting_value" not in app.config

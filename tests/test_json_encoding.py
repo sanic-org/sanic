5213cc4ccae31acac779a73929c8bd0abc3cd2b0
Adam Hopkins <admhpkns@gmail.com>
2021-03-10 10:32:15 +0200

    Add app level dumps change

---
+++ tests/test_json_encoding.py
@@ -0,0 +1,61 @@
+import sys
+
+from dataclasses import asdict, dataclass
+from json import dumps as sdumps
+from unittest.mock import patch
+
+import pytest
+
+from ujson import dumps as udumps
+
+from sanic import Sanic
+from sanic.response import BaseHTTPResponse, json
+
+
+@dataclass
+class Foo:
+    bar: str
+
+    def __json__(self):
+        return udumps(asdict(self))
+
+
+@pytest.fixture
+def foo():
+    return Foo(bar="bar")
+
+
+@pytest.fixture
+def payload(foo):
+    return {"foo": foo}
+
+
+@pytest.fixture(autouse=True)
+def default_back_to_ujson():
+    BaseHTTPResponse._dumps = udumps
+
+
+def test_change_encoder():
+    Sanic("...", dumps=sdumps)
+    assert BaseHTTPResponse._dumps == sdumps
+
+
+def test_json_response_ujson(payload):
+    response = json(payload)
+    assert response.body == b'{"foo":{"bar":"bar"}}'
+
+
+def test_json_response_json():
+    """One of the easiest ways to tell the difference is that ujson cannot
+    serialize over 64 bits"""
+    too_big_for_ujson = 111111111111111111111
+
+    with pytest.raises(OverflowError, match="int too big to convert"):
+        json(too_big_for_ujson)
+
+    response = json(too_big_for_ujson, dumps=sdumps)
+    assert sys.getsizeof(response.body) == 54
+
+    Sanic("...", dumps=sdumps)
+    response = json(too_big_for_ujson)
+    assert sys.getsizeof(response.body) == 54

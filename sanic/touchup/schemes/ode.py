1bde6cd3c82fd81afebf579822e5842fedffeac1
Adam Hopkins <adam@amhopkins.com>
2021-06-14 22:10:26 +0300

    Allow for dynamic ODE signals

--- sanic/touchup/schemes/ode.py
+++ sanic/touchup/schemes/ode.py
@@ -54,7 +54,20 @@ class OptionalDispatchEvent(BaseScheme):
             ):
                 event = expr.args[0]
                 event_name = getattr(event, "value", event.s)
-                if event_name not in self._registered_events:
+                if self._not_registered(event_name):
                     logger.debug(f"Disabling event: {event_name}")
                     return None
         return node
+
+    def _not_registered(self, event_name):
+        dynamic = []
+        for event in self._registered_events:
+            if event.endswith(">"):
+                namespace_concern, _ = event.rsplit(".", 1)
+                dynamic.append(namespace_concern)
+
+        namespace_concern, _ = event_name.rsplit(".", 1)
+        return (
+            event_name not in self._registered_events
+            and namespace_concern not in dynamic
+        )

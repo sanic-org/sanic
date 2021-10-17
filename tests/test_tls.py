f59006a3cb993a28bfd172e37b9a8591d1b2814f
L. Karkkainen <tronic@users.noreply.github.com>
2021-10-17 18:50:30 +0100

    Tests added for new TLS code.

--- tests/test_tls.py
+++ tests/test_tls.py
@@ -16,10 +16,13 @@ from sanic.response import text
 
 
 current_dir = os.path.dirname(os.path.realpath(__file__))
-localhost_cert = os.path.join(current_dir, "certs/localhost/fullchain.pem")
-localhost_key = os.path.join(current_dir, "certs/localhost/privkey.pem")
-sanic_cert = os.path.join(current_dir, "certs/sanic.example/fullchain.pem")
-sanic_key = os.path.join(current_dir, "certs/sanic.example/privkey.pem")
+localhost_dir = os.path.join(current_dir, "certs/localhost")
+sanic_dir = os.path.join(current_dir, "certs/sanic.example")
+invalid_dir = os.path.join(current_dir, "certs/invalid.nonexist")
+localhost_cert = os.path.join(localhost_dir, "fullchain.pem")
+localhost_key = os.path.join(localhost_dir, "privkey.pem")
+sanic_cert = os.path.join(sanic_dir, "fullchain.pem")
+sanic_key = os.path.join(sanic_dir, "privkey.pem")
 
 
 @pytest.mark.parametrize(
@@ -84,6 +87,50 @@ def test_url_attributes_with_ssl_dict(app, path, query, expected_url):
     assert parsed.netloc == request.host
 
 
+def test_cert_sni(app):
+    ssl_list = [localhost_dir, sanic_dir]
+
+    @app.get("/test")
+    async def handler(request):
+        return text(request.conn_info.server_name)
+
+    port = app.test_client.port
+    request, response = app.test_client.get(
+        f"https://localhost:{port}/test",
+        server_kwargs={"ssl": ssl_list},
+    )
+    assert response.status == 200
+    assert response.text == "localhost"
+
+    """The following requires foo.sanic.test 127.0.0.1 in hosts
+    port = app.test_client.port
+    request, response = app.test_client.get(
+        f"https://foo.sanic.test:{port}/test",
+        server_kwargs={"ssl": ssl_list},
+    )
+    assert response.status == 200
+    assert response.text == "foo.sanic.test"
+    """
+
+
+def test_missing_sni(app):
+    """The sanic cert does not list 127.0.0.1 and httpx does not send IP as SNI anyway."""
+    ssl_list = [sanic_dir]
+
+    @app.get("/")
+    async def handler(request):
+        return text(request.conn_info.server_name)
+
+    port = app.test_client.port
+    with pytest.raises(Exception) as exc:
+        request, response = app.test_client.get(
+            f"https://127.0.0.1:{port}/",
+            server_kwargs={"ssl": ssl_list},
+        )
+
+    assert "Request and response object expected" in str(exc.value)
+
+
 def test_invalid_ssl_dict(app):
     @app.get("/test")
     async def handler(request):
@@ -97,6 +144,85 @@ def test_invalid_ssl_dict(app):
         )
 
     assert str(excinfo.value) == "SSL dict needs filenames for cert and key."
+
+
+def test_invalid_ssl_type(app):
+    @app.get("/test")
+    async def handler(request):
+        return text("ssl test")
+
+    with pytest.raises(ValueError) as excinfo:
+        request, response = app.test_client.get(
+            "/test", server_kwargs={"ssl": False}
+        )
+
+    assert "Invalid ssl argument" in str(excinfo.value)
+
+
+def test_cert_file_on_pathlist(app):
+    @app.get("/test")
+    async def handler(request):
+        return text("ssl test")
+
+    ssl_list = [sanic_cert]
+
+    with pytest.raises(ValueError) as excinfo:
+        request, response = app.test_client.get(
+            "/test", server_kwargs={"ssl": ssl_list}
+        )
+
+    assert "folder expected" in str(excinfo.value)
+    assert sanic_cert in str(excinfo.value)
+
+
+def test_missing_cert_path(app):
+    @app.get("/test")
+    async def handler(request):
+        return text("ssl test")
+
+    ssl_list = [invalid_dir]
+
+    with pytest.raises(ValueError) as excinfo:
+        request, response = app.test_client.get(
+            "/test", server_kwargs={"ssl": ssl_list}
+        )
+
+    assert "not found" in str(excinfo.value)
+    assert invalid_dir + "/privkey.pem" in str(excinfo.value)
+
+
+def test_missing_cert_file(app):
+    @app.get("/test")
+    async def handler(request):
+        return text("ssl test")
+
+    invalid2 = invalid_dir.replace("nonexist", "certmissing")
+    ssl_list = [invalid2]
+
+    with pytest.raises(ValueError) as excinfo:
+        request, response = app.test_client.get(
+            "/test", server_kwargs={"ssl": ssl_list}
+        )
+
+    assert "not found" in str(excinfo.value)
+    assert invalid2 + "/fullchain.pem" in str(excinfo.value)
+
+
+def test_logger_vhosts(caplog):
+    app = Sanic(name=__name__)
+
+    @app.after_server_start
+    def stop(*args):
+        app.stop()
+
+    with caplog.at_level(logging.INFO):
+        app.run(host="127.0.0.1", port=42102, ssl=[localhost_dir, sanic_dir])
+
+    assert caplog.record_tuples[1] == (
+        "sanic.root",
+        logging.INFO,
+        "Certificate vhosts: localhost, 127.0.0.1, 0:0:0:0:0:0:0:1, sanic.example, www.sanic.example, *.sanic.test, 2001:DB8:0:0:0:0:0:541C, localhost",
+    )
 
 
 @pytest.mark.skipif(

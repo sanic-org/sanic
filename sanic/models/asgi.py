f7456cf6982bd776d2265d03b2b231c846e5c998
Stephen Sadowski <stephen.sadowski@sjsadowski.com>
2021-10-28 16:06:51 -0500

    Added branching statement to hanle asyncio.Event() loop parameter removal in 3.10, and optional supply in 3.9

--- sanic/models/asgi.py
+++ sanic/models/asgi.py
@@ -15,12 +15,21 @@ ASGIReceive = Callable[[], Awaitable[ASGIMessage]]
 
 class MockProtocol:
     def __init__(self, transport: "MockTransport", loop):
+        # This should be refactored when < 3.8 support is dropped
         self.transport = transport
         # Fixup for 3.8+; Sanic still supports 3.7 where loop is required
         loop=loop if sys.version_info[:2] < (3, 8) else None
-        self._not_paused = asyncio.Event(loop=loop)
-        self._not_paused.set()
-        self._complete = asyncio.Event(loop=loop)
+        # Arjan style - minimizing indents
+        # Optional in 3.9, necessary in 3.10 because the parameter "loop"
+        # was completely removed
+        if not loop:
+            self._not_paused = asyncio.Event()
+            self._not_paused.set()
+            self._complete = asyncio.Event()
+        else:
+            self._not_paused = asyncio.Event(loop=loop)
+            self._not_paused.set()
+            self._complete = asyncio.Event(loop=loop)
 
     def pause_writing(self) -> None:
         self._not_paused.clear()

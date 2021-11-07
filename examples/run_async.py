230ecb008d3648ed82f615f1a1585c12caa86b95
Adam Hopkins <adam@amhopkins.com>
2021-11-07 20:27:43 +0200

    Fix examples to work as expected

--- examples/run_async.py
+++ examples/run_async.py
@@ -1,8 +1,11 @@
-from sanic import Sanic
-from sanic import response
-from signal import signal, SIGINT
 import asyncio
+
+from signal import SIGINT, signal
+
 import uvloop
+
+from sanic import Sanic, response
+
 
 app = Sanic(__name__)
 
@@ -11,12 +14,18 @@ app = Sanic(__name__)
 async def test(request):
     return response.json({"answer": "42"})
 
+
 asyncio.set_event_loop(uvloop.new_event_loop())
-server = app.create_server(host="0.0.0.0", port=8000, return_asyncio_server=True)
+server = app.create_server(
+    host="0.0.0.0", port=8000, return_asyncio_server=True
+)
 loop = asyncio.get_event_loop()
 task = asyncio.ensure_future(server)
+server = loop.run_until_complete(task)
+loop.run_until_complete(server.startup())
 signal(SIGINT, lambda s, f: loop.stop())
+
 try:
     loop.run_forever()
-except:
+finally:
     loop.stop()
--- examples/run_async_advanced.py
+++ examples/run_async_advanced.py
@@ -11,9 +11,24 @@ from sanic.server import AsyncioServer
 app = Sanic(__name__)
 
 
-@app.listener("after_server_start")
-async def after_start_test(app, loop):
-    print("Async Server Started!")
+@app.before_server_start
+async def before_server_start(app, loop):
+    print("Async Server starting")
+
+
+@app.after_server_start
+async def after_server_start(app, loop):
+    print("Async Server started")
+
+
+@app.before_server_stop
+async def before_server_stop(app, loop):
+    print("Async Server stopping")
+
+
+@app.after_server_stop
+async def after_server_stop(app, loop):
+    print("Async Server stopped")
 
 
 @app.route("/")
@@ -28,20 +43,20 @@ serv_coro = app.create_server(
 loop = asyncio.get_event_loop()
 serv_task = asyncio.ensure_future(serv_coro, loop=loop)
 signal(SIGINT, lambda s, f: loop.stop())
-server: AsyncioServer = loop.run_until_complete(serv_task)  # type: ignore
-server.startup()
+server: AsyncioServer = loop.run_until_complete(serv_task)
+loop.run_until_complete(server.startup())
 
 # When using app.run(), this actually triggers before the serv_coro.
 # But, in this example, we are using the convenience method, even if it is
 # out of order.
-server.before_start()
-server.after_start()
+loop.run_until_complete(server.before_start())
+loop.run_until_complete(server.after_start())
 try:
     loop.run_forever()
 except KeyboardInterrupt:
     loop.stop()
 finally:
-    server.before_stop()
+    loop.run_until_complete(server.before_stop())
 
     # Wait for server to close
     close_task = server.close()
@@ -50,4 +65,4 @@ finally:
     # Complete all tasks on the loop
     for connection in server.connections:
         connection.close_if_idle()
-    server.after_stop()
+    loop.run_until_complete(server.after_stop())

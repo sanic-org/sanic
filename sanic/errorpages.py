1f23106fa83418e216388aa2e86b68beefb556a8
Adam Hopkins <adam@amhopkins.com>
2021-11-21 13:55:19 +0200

    Cleanup mistaken print statement

--- sanic/errorpages.py
+++ sanic/errorpages.py
@@ -383,7 +383,6 @@ def exception_response(
     """
     content_type = None
 
-    print("exception_response", fallback)
     if not renderer:
         # Make sure we have something set
         renderer = base

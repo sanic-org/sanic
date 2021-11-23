55c36e0240dfeb03deccdeb5a53ca7fcfa728bff
Adam Hopkins <admhpkns@gmail.com>
2021-11-23 23:00:25 +0200

    Fix examples to work as expected (#2305)
    * Fix examples to work as expected
    
    * Clean up examples
    
    * Update worker test
    
    * Merge in from main and cleanup example

--- examples/delayed_response.py
+++ examples/delayed_response.py
@@ -2,11 +2,14 @@ from asyncio import sleep
 
 from sanic import Sanic, response
 
+
 app = Sanic(__name__, strict_slashes=True)
+
 
 @app.get("/")
 async def handler(request):
     return response.redirect("/sleep/3")
+
 
 @app.get("/sleep/<t:number>")
 async def handler2(request, t=0.3):
@@ -14,5 +17,5 @@ async def handler2(request, t=0.3):
     return response.text(f"Slept {t:.1f} seconds.\n")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)

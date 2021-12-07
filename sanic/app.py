426742b3e2051425cfd88b1e492234c97ebb06b0
Zhiwei <43905414+ChihweiLHBird@users.noreply.github.com>
2021-12-07 03:10:04 -0700

    Fix `Sanic.handle_exception` Method's Type Doc (#2333)
    * Fix type doc
    
    * Remove explicit type defines in docstring

--- sanic/app.py
+++ sanic/app.py
@@ -724,9 +724,7 @@ class Sanic(BaseSanic, metaclass=TouchUpMeta):
         A handler that catches specific exceptions and outputs a response.
 
         :param request: The current request object
-        :type request: :class:`SanicASGITestClient`
         :param exception: The exception that was raised
-        :type exception: BaseException
         :raises ServerError: response 500
         """
         await self.dispatch(

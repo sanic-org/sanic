3c58c09f2c6bf2da910f682530d6ba7590b5ca08
prryplatypus <25409753+prryplatypus@users.noreply.github.com>
2021-12-08 17:53:26 +0100

    Added missing tests

--- tests/test_server_loop.py
+++ tests/test_server_loop.py
@@ -1,4 +1,7 @@
 import logging
+
+from unittest.mock import Mock, patch
+
 import pytest
 
 from sanic.server import loop
@@ -27,23 +30,82 @@ def test_raises_warning_if_os_is_windows(caplog):
 
 
 @pytest.mark.skipif(
-    OS_IS_WINDOWS,
+    OS_IS_WINDOWS or UVLOOP_INSTALLED,
     reason="Not testable with current client",
 )
 def test_raises_warning_if_uvloop_not_installed(caplog):
-    if not UVLOOP_INSTALLED:
-        with caplog.at_level(logging.WARNING):
-            loop.try_use_uvloop()
+    with caplog.at_level(logging.WARNING):
+        loop.try_use_uvloop()
 
-        for record in caplog.records:
-            if record.message.startswith("You are trying to use"):
-                break
+    for record in caplog.records:
+        if record.message.startswith("You are trying to use"):
+            break
 
-        assert record.message == (
-            "You are trying to use uvloop, but uvloop is not "
-            "installed in your system. In order to use uvloop "
-            "you must first install it. Otherwise, you can disable "
-            "uvloop completely by setting the 'USE_UVLOOP' "
-            "configuration value to false. Sanic will now continue "
-            "to run with the default event loop."
-        )
+    assert record.message == (
+        "You are trying to use uvloop, but uvloop is not "
+        "installed in your system. In order to use uvloop "
+        "you must first install it. Otherwise, you can disable "
+        "uvloop completely by setting the 'USE_UVLOOP' "
+        "configuration value to false. Sanic will now continue "
+        "to run with the default event loop."
+    )
+
+
+@pytest.mark.skipif(
+    OS_IS_WINDOWS or not UVLOOP_INSTALLED,
+    reason="Not testable with current client",
+)
+def test_logs_when_install_and_runtime_config_mismatch(caplog, monkeypatch):
+    getenv = Mock(return_value="no")
+    monkeypatch.setattr(loop, "getenv", getenv)
+
+    with caplog.at_level(logging.INFO):
+        loop.try_use_uvloop()
+
+    assert getenv.assert_called_once_with("SANIC_NO_UVLOOP", "no")
+    assert caplog.record_tuples == []
+
+    getenv.reset_mock(return_value="yes")
+    with caplog.at_level(logging.INFO):
+        loop.try_use_uvloop()
+
+    assert getenv.assert_called_once_with("SANIC_NO_UVLOOP", "no")
+    for record in caplog.records:
+        if record.message.startswith("You are requesting to run"):
+            break
+
+    assert record.message == (
+        "You are requesting to run Sanic using uvloop, but the "
+        "install-time 'SANIC_NO_UVLOOP' environment variable (used to "
+        "opt-out of installing uvloop with Sanic) is set to true. If "
+        "you want to prevent Sanic from overriding the event loop policy "
+        "during runtime, set the 'USE_UVLOOP' configuration value to "
+        "false."
+    )
+
+
+@pytest.mark.skipif(
+    OS_IS_WINDOWS or not UVLOOP_INSTALLED,
+    reason="Not testable with current client",
+)
+def test_sets_loop_policy_only_when_not_already_set(monkeypatch):
+    import uvloop  # type: ignore
+
+    get_event_loop_policy = Mock(return_value=None)
+    monkeypatch.setattr(
+        loop.asyncio, "get_event_loop_policy", get_event_loop_policy
+    )
+
+    with patch("asyncio.set_event_loop_policy") as set_event_loop_policy:
+        # Existing policy is not uvloop.EventLoopPolicy
+        loop.try_use_uvloop()
+        set_event_loop_policy.assert_called_once()
+        policy = set_event_loop_policy.call_args[0]
+        assert isinstance(policy, uvloop.EventLoopPolicy)
+
+        get_event_loop_policy.reset_mock(return_value=policy)
+        set_event_loop_policy.reset_mock()
+
+        # Existing policy is uvloop.EventLoopPolicy
+        loop.try_use_uvloop()
+        set_event_loop_policy.assert_not_called()

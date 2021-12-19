7493edb9d8def88127d1a5a85183fece953fbf4d
Adam Hopkins <adam@amhopkins.com>
2021-12-19 23:30:26 +0200

    skip shutdown tasks on 3.7

--- sanic/server/runners.py
+++ sanic/server/runners.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+import sys
+
 from ssl import SSLContext
 from typing import TYPE_CHECKING, Dict, Optional, Type, Union
 
@@ -174,7 +176,8 @@ def serve(
             loop.run_until_complete(asyncio.sleep(0.1))
             start_shutdown = start_shutdown + 0.1
 
-        app.shutdown_tasks(graceful - start_shutdown)
+        if sys.version_info > (3, 7):
+            app.shutdown_tasks(graceful - start_shutdown)
 
         # Force close non-idle connection after waiting for
         # graceful_shutdown_timeout

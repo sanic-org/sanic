c923dbfb587a7f2135beae08b28d4a06d60ab186
Javier Marcet <javier@marcet.info>
2022-01-24 16:18:53 +0100

    Fix "DeprecationWarning: There is no current event loop"
    Signed-off-by: Javier Marcet <javier@marcet.info>

--- sanic/app.py
+++ sanic/app.py
@@ -12,6 +12,7 @@ from asyncio import (
     Task,
     ensure_future,
     get_event_loop,
+    get_event_loop_policy,
     get_running_loop,
     wait_for,
 )
@@ -254,7 +255,7 @@ class Sanic(BaseSanic, RunnerMixin, metaclass=TouchUpMeta):
                 "Loop can only be retrieved after the app has started "
                 "running. Not supported with `create_server` function"
             )
-        return get_event_loop()
+        return get_event_loop_policy().get_event_loop()
 
     # -------------------------------------------------------------------- #
     # Registration

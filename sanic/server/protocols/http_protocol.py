372e1944c9fa84fd8929cf1572afe5c7dcce2cf6
Ryu juheon <saidbysolo@gmail.com>
2022-03-28 00:21:40 +0900

    fix: add ``=``

--- sanic/server/protocols/http_protocol.py
+++ sanic/server/protocols/http_protocol.py
@@ -173,7 +173,7 @@ class HttpProtocol(SanicProtocol, metaclass=TouchUpMeta):
                 return
             cancel_msg_args = ()
             if sys.version_info >= (3, 9):
-                cancel_msg_args("Cancel connection task with a timeout",)
+                cancel_msg_args = ("Cancel connection task with a timeout",)
             self._task.cancel(*cancel_msg_args)
         except Exception:
             error_logger.exception("protocol.check_timeouts")

8ca621691634fb5271a67bda4775fdc8cce7f6f8
Stephen Sadowski <stephen.sadowski@sjsadowski.com>
2022-04-22 13:08:44 -0500

    task(request): per discussion - remove getter/setter

--- sanic/request.py
+++ sanic/request.py
@@ -93,7 +93,7 @@ class Request:
         "_socket",
         "_match_info",
         "_name",
-        "_keep_blank_form_values",
+        "keep_blank_form_values",
         "app",
         "body",
         "conn_info",
@@ -146,7 +146,7 @@ class Request:
         self.head = head
 
         # enforce existing behavior prior to issue #2427
-        self.keep_blank_form_values = False
+        self.keep_blank_form_values: bool = False
 
         # Init but do not inhale
         self.body = b""
@@ -431,14 +431,6 @@ class Request:
         return self.parsed_credentials
 
     @property
-    def keep_blank_form_values(self) -> bool:
-        return self._keep_blank_form_values
-
-    @keep_blank_form_values.setter
-    def keep_blank_values(self, keep_blank_form_values: bool = False):
-        self._keep_blank_form_values = keep_blank_form_values
-
-    @property
     def form(self):
         if self.parsed_form is None:
             self.parsed_form = RequestParameters()
@@ -451,7 +443,8 @@ class Request:
                 if content_type == "application/x-www-form-urlencoded":
                     self.parsed_form = RequestParameters(
                         parse_qs(
-                            self.body.decode("utf-8"), self.keep_blank_form_values
+                            self.body.decode("utf-8"),
+                            self.keep_blank_form_values,
                         )
                     )
                 elif content_type == "multipart/form-data":

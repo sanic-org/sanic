6b9edfd05cbe3dd13136d1ad97a248dd5f8751dd
Santi Cardozo <57201631+scardozos@users.noreply.github.com>
2022-10-25 15:54:44 +0200

    improve error message if no apps found in registry (#2585)

--- sanic/app.py
+++ sanic/app.py
@@ -1453,7 +1453,14 @@ class Sanic(BaseSanic, StartupMixin, metaclass=TouchUpMeta):
                 return cls.get_app("__mp_main__", force_create=force_create)
             if force_create:
                 return cls(name)
-            raise SanicException(f'Sanic app name "{name}" not found.')
+            raise SanicException(
+                f"Sanic app name '{name}' not found.\n"
+                "App instantiation must occur outside "
+                "if __name__ == '__main__' "
+                "block or by using an AppLoader.\nSee "
+                "https://sanic.dev/en/guide/deployment/app-loader.html"
+                " for more details."
+            )
 
     @classmethod
     def _check_uvloop_conflict(cls) -> None:

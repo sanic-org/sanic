662063e13741f42f577a96812a206017b6f25505
prryplatypus <25409753+prryplatypus@users.noreply.github.com>
2022-10-29 20:37:41 +0200

    Add tests layout

---
+++ tests/test_response_json.py
@@ -0,0 +1,33 @@
+JSON_DATA = {"ok": True}
+
+
+def test_body_can_be_retrieved():
+    ...
+
+
+def test_body_can_be_set():
+    ...
+
+
+def test_raw_body_can_be_retrieved():
+    ...
+
+
+def test_raw_body_can_be_set():
+    ...
+
+
+def test_raw_body_cant_be_retrieved_after_body_set():
+    ...
+
+
+def test_raw_body_can_be_reset_after_body_set():
+    ...
+
+
+def test_set_json():
+    ...
+
+
+def test_set_json_after_body_set():
+    ...

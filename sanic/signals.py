7d3e6dfcf5fbe1059688132194e8513b0475c1a3
Zhiwei Liang <zhi.wei.liang@outlook.com>
2022-12-07 05:19:08 -0600

    Add deprecation comment

--- sanic/signals.py
+++ sanic/signals.py
@@ -218,8 +218,9 @@ class SignalRouter(BaseRouter):
             event = ".".join([*parts[:2], "<__trigger__>"])
 
         try:
-            handler.__requirements__ = condition # type: ignore
-            handler.__trigger__ = trigger # type: ignore
+            # Attaching __requirements__ and __trigger__ to the handler is deprecated and will be removed in v23.6.
+            handler.__requirements__ = condition  # type: ignore
+            handler.__trigger__ = trigger  # type: ignore
         except AttributeError:
             pass
 

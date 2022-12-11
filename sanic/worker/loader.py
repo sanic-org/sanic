a620ff2f5062a946b27cf26b04a223a348b65817
Adam Hopkins <adam@amhopkins.com>
2022-12-11 13:02:19 +0200

    Add app clear to the loader on factory

--- sanic/worker/loader.py
+++ sanic/worker/loader.py
@@ -23,6 +23,7 @@ class AppLoader:
         as_simple: bool = False,
         args: Any = None,
         factory: Optional[Callable[[], SanicApp]] = None,
+        clear_apps_on_factory: bool = True,
     ) -> None:
         self.module_input = module_input
         self.module_name = ""
@@ -31,6 +32,7 @@ class AppLoader:
         self.as_simple = as_simple
         self.args = args
         self.factory = factory
+        self.clear_apps = clear_apps_on_factory
         self.cwd = os.getcwd()
 
         if module_input:
@@ -47,6 +49,11 @@ class AppLoader:
         module_path = os.path.abspath(self.cwd)
         if module_path not in sys.path:
             sys.path.append(module_path)
+
+        if self.clear_apps and self.factory or self.as_factory:
+            from sanic import Sanic
+
+            Sanic._app_registry = {}
 
         if self.factory:
             return self.factory()

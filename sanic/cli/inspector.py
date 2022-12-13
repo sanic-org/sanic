42c44677543b4e115e77fe6bc0a89e17124affd1
Adam Hopkins <adam@amhopkins.com>
2022-12-14 00:46:32 +0200

    Flex keyord args

--- sanic/cli/inspector.py
+++ sanic/cli/inspector.py
@@ -1,9 +1,10 @@
 from argparse import ArgumentParser
 
+from sanic.application.logo import get_logo
 from sanic.cli.base import SanicHelpFormatter, SanicSubParsersAction
 
 
-def make_inspector_parser(parser: ArgumentParser) -> None:
+def _add_shared(parser: ArgumentParser) -> None:
     parser.add_argument("--host", "-H", default="localhost")
     parser.add_argument("--port", "-p", default=6457, type=int)
     parser.add_argument("--secure", "-s", action="store_true")
@@ -13,6 +14,17 @@ def make_inspector_parser(parser: ArgumentParser) -> None:
         help="Whether to output the raw response information",
     )
 
+
+class InspectorSubParser(ArgumentParser):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        _add_shared(self)
+        if not self.description:
+            self.description = get_logo(True)
+
+
+def make_inspector_parser(parser: ArgumentParser) -> None:
+    _add_shared(parser)
     subparsers = parser.add_subparsers(
         action=SanicSubParsersAction,
         dest="action",
@@ -21,15 +33,24 @@ def make_inspector_parser(parser: ArgumentParser) -> None:
             "Inspector instance, then you can run custom commands.\nSee ___ "
             "for more details."
         ),
-        title="Subcommands",
+        title="Required\n========\n  Subcommands",
+        parser_class=InspectorSubParser,
     )
     subparsers.add_parser(
-        "reload", help="Trigger a reload of the server workers"
+        "reload",
+        help="Trigger a reload of the server workers",
+        formatter_class=SanicHelpFormatter,
     )
     subparsers.add_parser(
-        "shutdown", help="Shutdown the application and all processes"
+        "shutdown",
+        help="Shutdown the application and all processes",
+        formatter_class=SanicHelpFormatter,
     )
-    scale = subparsers.add_parser("scale", help="Scale the number of workers")
+    scale = subparsers.add_parser(
+        "scale",
+        help="Scale the number of workers",
+        formatter_class=SanicHelpFormatter,
+    )
     scale.add_argument("replicas", type=int)
 
     custom = subparsers.add_parser(

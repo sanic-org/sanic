a6ff13ceeda409cf113b94c16818290ed96e1e42
L. Karkkainen <tronic@users.noreply.github.com>
2023-02-05 16:06:45 +0000

    Fallback for route name when no route found.

--- sanic/pages/error.py
+++ sanic/pages/error.py
@@ -29,6 +29,10 @@ class ErrorPage(BasePage):
 
     def _body(self) -> None:
         debug = self.request.app.debug
+        try:
+            route_name = self.request.route.name
+        except AttributeError:
+            route_name = "[route not found]"
         with self.doc.main:
             self.doc.h1(f"⚠️ {self.title}").p(self.text)
             context = getattr(self.exc, "context", None) or {}
@@ -46,7 +50,7 @@ class ErrorPage(BasePage):
             with self.doc.details(open=self.full, class_="smalltext"):
                 self.doc.summary("Details for developers (Sanic debug mode only)")
                 if self.exc:
-                    self.doc.h2(f"Exception in {self.request.route.name}:")
+                    self.doc.h2(f"Exception in {route_name}:")
                     # skip_outmost=1 to hide Sanic.handle_request
                     self.doc(html_traceback(self.exc, skip_outmost=1, include_js_css=False))
 

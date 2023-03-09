c695c5250a9f29dd3888224170cf57e30c01cbd5
Adam Hopkins <adam@amhopkins.com>
2023-03-09 10:37:41 +0200

    squash

--- sanic/__version__.py
+++ sanic/__version__.py
@@ -5,6 +5,6 @@ from inspect import currentframe, stack
 
 for frame_info in stack():
     if frame_info.frame is not currentframe():
-        for member, value in frame_info.frame.f_globals.items():
-            if member.startswith("__") and member.isupper():
-                __compatibility__ = value
+        value = frame_info.frame.f_globals.get("__SANIC_COMPATIBILITY__")
+        if value:
+            __compatibility__ = value

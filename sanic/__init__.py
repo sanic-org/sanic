71cd53b64ec2d39a66feb8e256eeeea4ae332deb
Adam Hopkins <adam@amhopkins.com>
2023-03-20 14:50:50 +0200

    Standardize init of exceptions (#2545)

--- sanic/__init__.py
+++ sanic/__init__.py
@@ -2,6 +2,22 @@ from sanic.__version__ import __version__
 from sanic.app import Sanic
 from sanic.blueprints import Blueprint
 from sanic.constants import HTTPMethod
+from sanic.exceptions import (
+    BadRequest,
+    ExpectationFailed,
+    FileNotFound,
+    Forbidden,
+    HeaderNotFound,
+    InternalServerError,
+    InvalidHeader,
+    MethodNotAllowed,
+    NotFound,
+    RangeNotSatisfiable,
+    SanicException,
+    ServerError,
+    ServiceUnavailable,
+    Unauthorized,
+)
 from sanic.request import Request
 from sanic.response import (
     HTTPResponse,
@@ -9,6 +25,7 @@ from sanic.response import (
     file,
     html,
     json,
+    raw,
     redirect,
     text,
 )
@@ -17,16 +34,34 @@ from sanic.server.websockets.impl import WebsocketImplProtocol as Websocket
 
 __all__ = (
     "__version__",
+    # Common objects
     "Sanic",
     "Blueprint",
     "HTTPMethod",
     "HTTPResponse",
     "Request",
     "Websocket",
+    # Common exceptions
+    "BadRequest",
+    "ExpectationFailed",
+    "FileNotFound",
+    "Forbidden",
+    "HeaderNotFound",
+    "InternalServerError",
+    "InvalidHeader",
+    "MethodNotAllowed",
+    "NotFound",
+    "RangeNotSatisfiable",
+    "SanicException",
+    "ServerError",
+    "ServiceUnavailable",
+    "Unauthorized",
+    # Common response methods
     "empty",
     "file",
     "html",
     "json",
+    "raw",
     "redirect",
     "text",
 )

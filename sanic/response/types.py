875e921bdafb84aa4043bb55ac2a1bf49ac5a1a0
L. Kärkkäinen <Tronic@users.noreply.github.com>
2023-09-20 19:59:35 +0100

    Do not strip entity-headers.

--- sanic/response/types.py
+++ sanic/response/types.py
@@ -106,9 +106,6 @@ class BaseHTTPResponse:
         Returns:
             Iterator[Tuple[bytes, bytes]]: A list of header tuples encoded in bytes for sending
         """  # noqa: E501
-        # TODO: Make a blacklist set of header names and then filter with that
-        if self.status in (304, 412):  # Not Modified, Precondition Failed
-            self.headers = remove_entity_headers(self.headers)
         if has_message_body(self.status):
             self.headers.setdefault("content-type", self.content_type)
         # Encode headers into bytes

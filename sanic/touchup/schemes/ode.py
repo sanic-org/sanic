0bc7189b3100ae7b19947ce519d90bf15d7ea187
hamed <hamed.sheykglu@gmail.com>
2024-03-14 11:28:53 +0300

    fix DeprecationWarning: Attribute s is deprecated and will be removed in Python 3.14; use value instead
    The attribute check within the 'ode.py' script has been updated. Previously, it checked whether the event has an attribute 's', however, this has been modified to check for the 'value' attribute instead. This ensures the correct attribute is being checked for the ongoing processes.

--- sanic/touchup/schemes/ode.py
+++ sanic/touchup/schemes/ode.py
@@ -67,7 +67,7 @@ class RemoveDispatch(NodeTransformer):
 
         if isinstance(func, Attribute) and func.attr == "dispatch":
             event = args[0]
-            if hasattr(event, "s"):
+            if hasattr(event, "value"):
                 event_name = getattr(event, "value", event.s)
                 if self._not_registered(event_name):
                     logger.debug(

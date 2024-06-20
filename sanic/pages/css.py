a704989389cd9db829ae86b135a8f36ef3b68b50
Ayush <ayushkhadkak@gmail.com>
2024-06-20 14:06:52 +0200

    Added Branch Instrumentation

--- sanic/pages/css.py
+++ sanic/pages/css.py
@@ -5,16 +5,30 @@ from typing import Optional
 
 CURRENT_DIR = Path(__file__).parent
 
+branch_coverage = {
+    "cssBranch1": False,  
+    "cssBranch2": False,
+    "cssBranch3": False,
+    "cssBranch4": False,
+    "cssBranch5": False,
+    "cssBranch6": False,
+}
 
 def _extract_style(maybe_style: Optional[str], name: str) -> str:
     if maybe_style is not None:
+        branch_coverage["cssBranch1"] = True
         maybe_path = Path(maybe_style)
         if maybe_path.exists():
+            branch_coverage["cssBranch2"] = True
             return maybe_path.read_text(encoding="UTF-8")
+        branch_coverage["cssBranch3"] = True
         return maybe_style
+    branch_coverage["cssBranch4"] = True
     maybe_path = CURRENT_DIR / "styles" / f"{name}.css"
     if maybe_path.exists():
+        branch_coverage["cssBranch5"] = True
         return maybe_path.read_text(encoding="UTF-8")
+    branch_coverage["cssBranch6"] = True
     return ""
 
 

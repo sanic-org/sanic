c26a47b41586c6c616da4df945cde97bf2cd21ec
Ayush <ayushkhadkak@gmail.com>
2024-06-22 23:38:55 +0200

    better function names for extract style

--- tests/test_css.py
+++ tests/test_css.py
@@ -6,18 +6,18 @@ def test_initial_print_extract_style():
     print_extract_style_coverage()
     print("\n")
 
-def expected_file_test():
+def expected_file_path_test():
     expected_file = "p {text-align: center; color: red;} "
     Path("test_style.css").write_text(expected_file, encoding="UTF-8")
     results = _extract_style("test_style.css", "test")
     assert results == expected_file, f"Expected {expected_file},{results}"
 
-def non_existing_file_test():
+def non_existing_file_path_test():
     non_existingFile = "nonexistent_style.css"
     results = _extract_style(non_existingFile, "test")
     assert results == non_existingFile, f"Expected {non_existingFile},{results}"
 
-def no_files_Test():
+def no_files_path_test():
     results = _extract_style(None, "nonexistent_page")
     assert results == "", f"Expected '',{results}"
 
@@ -27,7 +27,7 @@ def test_final_print_extract_style():
     print("\n")
 
 test_initial_print_extract_style()
-expected_file_test()
-non_existing_file_test()
-no_files_Test()
+expected_file_path_test()
+non_existing_file_path_test()
+no_files_path_test()
 test_final_print_extract_style()

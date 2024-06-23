e00fb0393cb596f6956f8703cc148cb4f6828385
MedonAbraham <medonabraham@gmail.com>
2024-06-23 11:57:36 +0200

    Revert "Merge pull request #3 from janaabuasbeh/amir-branch"
    This reverts commit 8bfe212c484be8c37782dec76d65b5e881ce0c60, reversing
    changes made to 41ff52df30deddf3c2d34a7c4e1869926174568e.

--- sanic/app.py
+++ sanic/app.py
@@ -113,9 +113,6 @@ if OS_IS_WINDOWS:  # no cov
 ctx_type = TypeVar("ctx_type")
 config_type = TypeVar("config_type", bound=Config)
 
-refresh_branch_coverage = {"refresh_b1" : False, "refresh_b2" : False, "refresh_b3" : False, "refresh_b4" : False}
-purge_branch_coverage = {"purge_b1" : False, "purge_b2" : False}
-
 
 class Sanic(
     Generic[config_type, ctx_type],
@@ -1877,21 +1874,13 @@ class Sanic(
         """
         for key, task in self._task_registry.items():
             if task is None:
-                purge_branch_coverage["purge_b1"] = True
                 continue
             if task.done() or task.cancelled():
-                purge_branch_coverage["purge_b1"] = True
                 self._task_registry[key] = None
 
         self._task_registry = {
             k: v for k, v in self._task_registry.items() if v is not None
         }
-
-    def print_apply_coverage():
-        for branch, hit in purge_branch_coverage.items():
-            print(f"{branch} was {'hit' if hit else 'not hit'}")
-
-    print_apply_coverage()
 
     def shutdown_tasks(
         self, timeout: Optional[float] = None, increment: float = 0.1
@@ -2490,13 +2479,10 @@ class Sanic(
         """  # noqa: E501
         registered = self.__class__.get_app(self.name)
         if self is not registered:
-            refresh_branch_coverage["refresh_b1"] = True
             if not registered.state.server_info:
-                refresh_branch_coverage["refresh_b2"] = True
                 registered.state.server_info = self.state.server_info
             self = registered
         if passthru:
-            refresh_branch_coverage["refresh_b3"] = True
             for attr, info in passthru.items():
                 if isinstance(info, dict):
                     for key, value in info.items():
@@ -2504,15 +2490,8 @@ class Sanic(
                 else:
                     setattr(self, attr, info)
         if hasattr(self, "multiplexer"):
-            refresh_branch_coverage["refresh_b4"] = True
             self.shared_ctx.lock()
         return self
-
-    def print_refresh_coverage():
-        for branch, hit in refresh_branch_coverage.items():
-            print(f"{branch} was {'hit' if hit else 'not hit'}")
-    
-    print_refresh_coverage()
 
     @property
     def inspector(self) -> Inspector:
